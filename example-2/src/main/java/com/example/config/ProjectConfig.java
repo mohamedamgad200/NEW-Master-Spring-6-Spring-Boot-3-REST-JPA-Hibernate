ac98ec1103e3d465b2e853a63a1eb98ca0a28c13
Mohamed Amagd <125823369+mohamedamgad200@users.noreply.github.com>
2025-03-12 21:58:38 +0200

    Add files via upload

---
+++ example-2/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public Vehicle vehicle1(){
+        var veh=new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+    @Bean
+    public Vehicle vehicle2(){
+        var veh=new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+    @Bean
+    public Vehicle vehicle3(){
+        var veh=new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
