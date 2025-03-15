e1d229d6551326f233f1250f122d58cbc684fd2a
Mohamed Amagd <125823369+mohamedamgad200@users.noreply.github.com>
2025-03-15 14:18:54 +0200

    Add files via upload

---
+++ example-11/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,30 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name="Lucy";
+    @Autowired
+    private Vehicle vehicle;
+    public Person() {
+        System.out.println("Person bean created by spring");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
---
+++ example-9/src/main/java/com/example/main/Example9.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class Example9 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Person name from spring context is " + person.getName());
+        System.out.println("Vehicle name from spring context is " + vehicle.getName());
+        System.out.println("Vehicle that person own is " + person.getVehicle());
+    }
+}
