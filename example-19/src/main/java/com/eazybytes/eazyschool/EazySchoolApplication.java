3747b9745c643fbe0dbb1fba304e4440104e45bc
Mohamed Amagd <125823369+mohamedamgad200@users.noreply.github.com>
2025-03-30 17:07:07 +0200

    Add files via upload

---
+++ example-19/src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class EazySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolApplication.class, args);
+	}
+
+}
