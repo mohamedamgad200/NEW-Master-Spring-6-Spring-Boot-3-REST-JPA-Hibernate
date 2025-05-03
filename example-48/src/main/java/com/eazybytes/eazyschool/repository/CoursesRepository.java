7ee947fc806c7671e864ff2664ffe3985b1ac0ac
mohamedamgad <mishomishmish2@gmail.com>
2025-05-03 15:16:14 +0300

    Understand & Configuring logging inside SpringBoot

---
+++ example-48/src/main/java/com/eazybytes/eazyschool/repository/CoursesRepository.java
@@ -0,0 +1,18 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+//@RepositoryRestResource(exported = false)
+@RepositoryRestResource(path = "courses")
+public interface CoursesRepository extends JpaRepository<Courses,Integer> {
+    //Descending Order
+    List<Courses> findByOrderByNameDesc();
+    //Ascending Order
+    List<Courses> findByOrderByName();
+}
